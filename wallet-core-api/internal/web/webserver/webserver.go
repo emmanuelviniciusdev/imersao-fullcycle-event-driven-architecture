7fcf64cc0df78f6d4b61d5aa4f4e18380ad2943f
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2023-10-19 11:22:32 -0300

    imersao-fullcycle-event-driven-architecture

--- wallet-core-api/internal/web/webserver/webserver.go
+++ wallet-core-api/internal/web/webserver/webserver.go
@@ -8,27 +8,33 @@ import (
 )
 
 type WebServer struct {
-    Router chi.Router
-    Handlers map[string]http.HandlerFunc
-    WebServerPort string
+	Router        chi.Router
+	Handlers      map[string]http.HandlerFunc
+	WebServerPort string
 }
 
 func NewWebServer(webServerPort string) *WebServer {
-    return &WebServer{
-        Router: chi.NewRouter(),
-        Handlers: make(map[string]http.HandlerFunc),
-        WebServerPort: webServerPort,
-    }
+	return &WebServer{
+		Router:        chi.NewRouter(),
+		Handlers:      make(map[string]http.HandlerFunc),
+		WebServerPort: webServerPort,
+	}
 }
 
 func (s *WebServer) AddHandler(path string, handler http.HandlerFunc) {
-    s.Handlers[path] = handler
+	s.Handlers[path] = handler
 }
 
 func (s *WebServer) Start() {
-    s.Router.Use(middleware.Logger)
-    for path, handler := range s.Handlers {
-        s.Router.Post(path, handler)
-    }
-    http.ListenAndServe(s.WebServerPort, s.Router)
+	s.Router.Use(middleware.Logger)
+
+	for path, handler := range s.Handlers {
+		s.Router.Post(path, handler)
+	}
+
+	err := http.ListenAndServe(s.WebServerPort, s.Router)
+
+	if err != nil {
+		panic(err)
+	}
 }
